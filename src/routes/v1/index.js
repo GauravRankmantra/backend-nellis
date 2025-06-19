8fcae3c668ce3248df97e77f364d93822862b769
gauravrankmantra <gauravlakherarankmantra@gmail.com>
2025-06-19 10:17:50 +0530

    stopped at blog

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const router = express.Router();
 const dealerRouter = require("./dealership.router.js");
 const weeklySpclRouter = require("./weeklySpcl.router.js");
-const vehicleRouter=require("./vehicle.router.js")
-const specialOfferRouter = require("./specialOffer.router.js")
-const businessOfferRouter = require("./business.Router.js")
-
+const vehicleRouter = require("./vehicle.router.js");
+const specialOfferRouter = require("./specialOffer.router.js");
+const businessOfferRouter = require("./business.Router.js");
+const serviceRouter = require("./service.Router.js");
+const partsRouter = require("./parts.Router.js");
 
 router.use("/dealerships", dealerRouter);
 router.use("/weekly-specials", weeklySpclRouter);
 router.use("/vehicles", vehicleRouter);
-router.use("/special-offers",specialOfferRouter)
-router.use("/businesses",businessOfferRouter)
+router.use("/special-offers", specialOfferRouter);
+router.use("/businesses", businessOfferRouter);
+router.use("/services", serviceRouter);
+router.use("/parts", partsRouter);
 
 module.exports = router;
