bbd07c20903b5b58b567d252f8145abb186701c9
Styl659 <112603300+Styl659@users.noreply.github.com>
2025-06-23 16:28:39 +0530

    Create posts.js

---
+++ src/routes/v1/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { upload } = require('../../middlewares/multer.middleware.js');
+const postController = require('../../controllers/postController.js');
+
+router.get('/', postController.getAllPosts);
+router.get('/:id', postController.getPost);
+router.post('/', upload.fields([{ name: 'image', maxCount: 1 }]), postController.createPost);
+router.put('/:id', upload.fields([{ name: 'image', maxCount: 1 }]), postController.updatePost);
+router.delete('/:id', postController.deletePost);
+
+module.exports = router;
